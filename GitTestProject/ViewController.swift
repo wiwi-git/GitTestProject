bfb5b8690fea69f2a2f05e7cbc50d26d6ef318e9
wi <duse34@hotmail.com>
2021-02-23 18:38:39 +0900

    new branch

--- GitTestProject/ViewController.swift
+++ GitTestProject/ViewController.swift
@@ -13,7 +13,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("Hello world!")
     }
-
+    
+    func newBranchFunc() {
+        print("Hello branch")
+    }
 
 }
 
