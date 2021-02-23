3b77fa68cf4aca28d519bce26b389ea900c6f315
wi <duse34@hotmail.com>
2021-02-23 18:40:08 +0900

    master update 2

---
+++ GitTestProject/NewClass.swift
@@ -0,0 +1,13 @@
+//
+//  NewClass.swift
+//  GitTestProject
+//
+//  Created by 위대연 on 2021/02/23.
+//
+
+import Foundation
+class NewClass {
+    func newClassFunc() {
+        print("new class func")
+    }
+}
